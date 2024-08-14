6565c846b08031acf93cba04427230249b89c911
rsocorro17 <rsocorrogcf@gamil.com>
2024-08-14 19:40:36 -0400

    unit test

---
+++ src/test/java/com/challenge/springboot/api/water_bucket/WaterJugServiceTest.java
@@ -0,0 +1,49 @@
+package com.challenge.springboot.api.water_bucket;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.challenge.springboot.api.water_bucket.services.WaterJugService;
+import com.challenge.springboot.api.water_bucket.wrappers.SolutionWrapper;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class WaterJugServiceTest {
+    private WaterJugService waterJugService;
+
+    @BeforeEach
+    void setUp() {
+        waterJugService = new WaterJugService();
+    }
+
+    @Test
+    void testInvalidInputsThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> waterJugService.solve(0, 5, 10));
+        assertThrows(IllegalArgumentException.class, () -> waterJugService.solve(-1, 5, 10));
+        assertThrows(IllegalArgumentException.class, () -> waterJugService.solve(5, 0, 10));
+        assertThrows(IllegalArgumentException.class, () -> waterJugService.solve(0, 0, 10));
+    }
+
+    @Test
+    void testNoSolutionFoundForSomeCases() {
+        SolutionWrapper result = waterJugService.solve(3, 5, 7);
+        assertThat(result.getSolution()).isEmpty();
+        assertThat(result.getSolution().size()).isEqualTo(1);
+        Map<String, Object> statusMap = (Map<String, Object>) result.getSolution().get(0);
+        assertThat(statusMap).containsEntry("action", "No Solution Found");
+        assertThat(statusMap).containsEntry("status", "No Solution");
+    }
+
+    @Test
+    void testValidInputsWithSolution() {
+        SolutionWrapper result = waterJugService.solve(3, 5, 6);
+        assertThat(result.getSolution()).isNotEmpty();
+        
+    }
+
+}
